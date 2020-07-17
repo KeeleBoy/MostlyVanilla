6b76f3d3ea8dc4d063ef934afe1e02cd86c748a8
kyle <kwarchuck@gmail.com>
2020-07-17 18:50:57 -0400

    Basic start

---
+++ src/main/java/com/mostlyvanilla/zzzzz/mainController.java
@@ -0,0 +1,36 @@
+package com.mostlyvanilla.zzzzz;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+public class mainController {
+
+	@Controller
+	public class zombieController {
+		@Autowired
+		private HttpSession sesh;
+
+		@RequestMapping("/")
+		public ModelAndView home() {
+			ModelAndView mav = new ModelAndView("home");
+			return mav;
+		}
+
+		@RequestMapping("/page2")
+		public ModelAndView page2() {
+			ModelAndView mav = new ModelAndView("page2");
+			return mav;
+		}
+
+		@RequestMapping("/page3")
+		public ModelAndView page3() {
+			ModelAndView mav = new ModelAndView("page3");
+			return mav;
+		}
+	}
+
+}
